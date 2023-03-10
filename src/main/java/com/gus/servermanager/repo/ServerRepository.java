568128dc94dbe51a0eedddca2d4d60a2c9da4d1f
Gustavo <penarandagustavo5@gmail.com>
2023-03-10 11:27:40 -0500

    add ServerRepository

---
+++ src/main/java/com/gus/servermanager/repo/ServerRepository.java
@@ -0,0 +1,11 @@
+package com.gus.servermanager.repo;
+
+import com.gus.servermanager.domain.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+
+    Optional<Server> findByIpAddress(Server ipAddress);
+}
