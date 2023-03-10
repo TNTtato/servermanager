33995a07aed51d23ea2e7508cdad8f2cac143cc8
Gustavo <penarandagustavo5@gmail.com>
2023-03-10 11:19:53 -0500

    add Server entity and Status enum

---
+++ src/main/java/com/gus/servermanager/domain/Server.java
@@ -0,0 +1,33 @@
+package com.gus.servermanager.domain;
+
+import com.gus.servermanager.enumeration.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Server {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imgUrl;
+    private Status status;
+}
