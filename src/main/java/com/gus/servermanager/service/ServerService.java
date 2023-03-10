7e08dc51416ffa45c0157ce9abdf4a2faa90bbe5
Gustavo <penarandagustavo5@gmail.com>
2023-03-10 11:31:50 -0500

    define Service Service actions

---
+++ src/main/java/com/gus/servermanager/service/ServerService.java
@@ -0,0 +1,13 @@
+package com.gus.servermanager.service;
+
+import com.gus.servermanager.domain.Server;
+
+import java.util.Collection;
+
+public interface ServerService {
+    Server create(Server server);
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+}
